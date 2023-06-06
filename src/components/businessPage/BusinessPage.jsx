c635036d848c3a1823ee465a45747210a9237374
ebenezersakyi <ebensakyi0@gmail.com>
2023-06-06 21:21:27 +0000

    commit

--- src/components/businessPage/BusinessPage.jsx
+++ src/components/businessPage/BusinessPage.jsx
@@ -1,217 +1,245 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./BusinessPage.css";
 
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
-import axios from "axios";
+const containerStyle = {
+  width: "90vw",
+  height: "400px",
+};
 
-const Profile = ({ google }) => {
-  const [showDilogue, setShowDilogue] = useState(true);
-  const [inputValue, setInputValue] = useState("");
-  const [searchResults, setSetSearchResults] = useState([]);
-  const [searchingResults, setSetSearchingResults] = useState(false);
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 
-  const [selectedBusiness, setSelectedBusiness] = useState({});
+const Profile = () => {
+  const [showDilogue, setShowDilogue] = useState(false);
+  const [nameValue, setNameValue] = useState("");
+  const [cityAndCountry, setCityAndCountry] = useState("");
+  const [internationalPhone, setInternationalPhone] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [markerCoordinates, setMarkerCoordinates] = useState(null);
+
+  const [businessDetails, setBusinessDetails] = useState(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
 
   useEffect(() => {}, []);
 
-  useEffect(() => {
-    const delay = 2000;
-    let timerId;
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-    const debounceApiCall = () => {
-      clearTimeout(timerId); // Clear the previous timer
-      timerId = setTimeout(() => {
-        // Make your API call here using the searchTerm value
-        fetchBusinessInfo();
-        // console.log("API call with search term:", inputValue);
-      }, delay);
-    };
-    if (inputValue.length > 3) {
-      debounceApiCall();
-    } else {
-      setSetSearchResults([]);
-    }
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [inputValue]);
-
-  const fetchBusinessInfo = async () => {
-    setSetSearchingResults(true);
-    //   const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-
-    //   const placeId = "ChIJTXfPCEOb3w8R7oPKyy17Izg";
-    //   const url = `/api/maps/api/place/textsearch/json
-    // ?query=restaurants
-    // &key=AIzaSyBDHO8OgGVn4_ap5bsz7BRpWk-YpkZUK44`;
-
-    //   axios
-    //     .get(url)
-    //     .then((response) => {
-    //       console.log(response);
-    //     })
-    //     .catch((error) => {
-    //       console.log(error);
-    //     });
-
-    // let map = new google.maps.Map(document.getElementById("map"));
-
-    // var request = {
-    //   query: "Museum of Contemporary Art Australia",
-    //   fields: ["name", "geometry"],
-    // };
-
-    // var service = new google.maps.places.PlacesService(map);
-
-    // service.findPlaceFromQuery(request, function (results, status) {
-    //   if (status === google.maps.places.PlacesServiceStatus.OK) {
-    //     // for (var i = 0; i < results.length; i++) {
-    //     //   createMarker(results[i]);
-    //     // }
-    //     // map.setCenter(results[0].geometry.location);
-    //     console.log(results);
-    //   }
-    // });
-
-    // try {
-    //   const response = await axios.get(
-    //     `/api/maps/api/place/details/json?placeid=ChIJTXfPCEOb3w8R7oPKyy17Izg&key=AIzaSyBDHO8OgGVn4_ap5bsz7BRpWk-YpkZUK44`
-    //   );
-
-    //   if (response.status === 200 && response.data.status === "OK") {
-    //     // setPlace(response.data.result);
-    //     console.error("success");
-    //   }
-    // } catch (error) {
-    //   console.error("Error fetching place details:", error);
-    // }
-
-    const service = new google.maps.places.PlacesService(
-      document.createElement("div")
-    );
-    const request = {
-      query: inputValue,
-      // fields: ["name", "formatted_phone_number"],
+    reader.onload = () => {
+      setSelectedImage(reader.result);
     };
 
-    service.textSearch(request, (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        // Assuming the first result is the desired place
-        if (results && results.length > 0) {
-          // const place = results[0];
-          // setPlaceDetails({
-          //   name: place.name,
-          //   phoneNumber: place.formatted_phone_number,
-          //   openingHours: place.opening_hours,
-          // });
-          console.log(results);
-          setSetSearchResults(results);
-        } else {
-          setSetSearchingResults(false);
-        }
-      } else {
-        setSetSearchingResults(false);
-      }
-    });
+    reader.readAsDataURL(file);
   };
 
-  const inputChange = async (e) => {
-    setInputValue(e.target.value);
-  };
+  // const inputChange = async (e) => {
+  //   setInputValue(e.target.value);
+  // };
+
+  // const mapCenter = useMemo(() => {
+  //   return {  lat: -3.745,
+  //     lng: -38.523, }
+  // }, [])
 
   return (
     <div className="profile__container">
-      <div className="main__section">
+      <div className="main__section__buss">
         <span className="title__buss__pg">Enter the name of your business</span>
         <div
           className={
-            inputValue.length > 0 ? "search__div__active" : "search__div"
+            nameValue.length > 0 ? "search__div__active" : "search__div"
           }
         >
           <div className="input__section">
-            <span className="searchicon__div">
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-              </svg>
-            </span>
             <input
               className="search__input"
               placeholder="Enter a business name"
               type="text"
-              value={inputValue}
-              onChange={inputChange}
+              value={nameValue}
+              onChange={(e) => {
+                setNameValue(e.target.value);
+              }}
             />
           </div>
-          {searchResults.length > 0 ? (
-            <div className="results__container">
-              {searchResults.map((item, index) => (
-                <div key={index} className="business__item__div">
-                  <img
-                    className="business__item__img"
-                    src={item.icon}
-                    alt="business image"
-                  />
-                  <div className="details">
-                    <span className="buss__name">{item.name}</span>
-                    <span className="buss__loc">{item.formatted_address}</span>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ) : null}
         </div>
-      </div>
 
-      {showDilogue && (
-        <div className="display__alert__background">
-          <div className="display__alert">
-            <span>
-              Please note that at this time, we are only able to accept business
-              listings that are available in the Google Business API. This helps
-              us maintain the accuracy and reliability of the information
-              provided on our website. If your business is already listed on
-              Google, you can easily add it to our platform by following these
-              steps:
-            </span>
-            <li>
-              Search for your business using the same name and address as listed
-              on Google in the input below.
-            </li>
-            <li>Select your business from the search results.</li>
-            <li>
-              Verify your business email address. An OTP will be sent to the
-              email address associated with your business on Google. Retrieve
-              the OTP from your business email inbox. Enter the OTP on our
-              website to confirm ownership.
-            </li>
-            <li>
-              Verify the details and complete any additional information
-              required.
-            </li>
-            <li>Submit your business for review.</li>
-
-            <span>
-              If your business is not yet listed on Google, we recommend
-              creating a Google My Business listing first. Once your business is
-              available in the Google Business API, you can come back to our
-              website and add it to our platform.
-            </span>
-            <button
-              onClick={() => setShowDilogue(false)}
-              className="close__button"
-            >
-              Close
-            </button>
+        <span className="title__buss__pg">Enter a city and country</span>
+        <div
+          className={
+            cityAndCountry.length > 0 ? "search__div__active" : "search__div"
+          }
+        >
+          <div className="input__section">
+            <input
+              className="search__input"
+              placeholder="Eg: Accra, Ghana"
+              type="text"
+              value={cityAndCountry}
+              onChange={(e) => {
+                setCityAndCountry(e.target.value);
+              }}
+            />
           </div>
         </div>
-      )}
+
+        <span className="title__buss__pg">
+          Enter the international phone number of your business
+        </span>
+        <div
+          className={
+            internationalPhone.length > 0
+              ? "search__div__active"
+              : "search__div"
+          }
+        >
+          <div className="input__section">
+            <input
+              className="search__input"
+              placeholder="Eg:  +233 123 123 123"
+              type="text"
+              value={internationalPhone}
+              onChange={(e) => {
+                setInternationalPhone(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <span className="title__buss__pg">
+          Enter a description of your business
+        </span>
+        <div
+          className={
+            description.length > 0 ? "search__div__active" : "search__div"
+          }
+        >
+          <div className="input__section">
+            <input
+              className="search__input"
+              placeholder="Enter a description"
+              type="text"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="image-selector">
+          <span className="title__buss__pg">
+            Choose your business profile picture
+          </span>
+          <label htmlFor="image-input" className="image-button">
+            Choose Image
+          </label>
+          <input
+            id="image-input"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+          {selectedImage && (
+            <img src={selectedImage} alt="Selected" className="thumbnail" />
+          )}
+        </div>
+
+        <>
+          <span className="title__buss__pg">
+            Drag the marker to the location of your business
+          </span>
+          {isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+              // onLoad={onLoad}
+              // onUnmount={onUnmount}
+            >
+              {/* Child components, such as markers, info windows, etc. */}
+              <Marker
+                onDrag={(data) =>
+                  setMarkerCoordinates({
+                    lat: data.latLng.lat(),
+                    lng: data.latLng.lng(),
+                  })
+                }
+                draggable={true}
+                position={{
+                  lat: -3.745,
+                  lng: -38.523,
+                }}
+              />
+            </GoogleMap>
+          ) : (
+            <></>
+          )}
+        </>
+      </div>
+      <button
+        className="submit-button"
+        // onClick={handleSubmit}
+      >
+        Submit
+      </button>
+
+      {/* {showDilogue && <Dialogue />} */}
     </div>
   );
+
+  function Dialogue() {
+    return (
+      <div className="display__alert__background">
+        <div className="display__alert">
+          <span>
+            Please note that at this time, we are only able to accept business
+            listings that are available in the Google Business API. This helps
+            us maintain the accuracy and reliability of the information provided
+            on our website. If your business is already listed on Google, you
+            can easily add it to our platform by following these steps:
+          </span>
+          <li>
+            Search for your business using the same name and address as listed
+            on Google in the input below.
+          </li>
+          <li>Select your business from the search results.</li>
+          <li>
+            Verify your business email address. An OTP will be sent to the email
+            address associated with your business on Google. Retrieve the OTP
+            from your business email inbox. Enter the OTP on our website to
+            confirm ownership.
+          </li>
+          <li>
+            Verify the details and complete any additional information required.
+          </li>
+          <li>Submit your business for review.</li>
+
+          <span>
+            If your business is not yet listed on Google, we recommend creating
+            a Google My Business listing first. Once your business is available
+            in the Google Business API, you can come back to our website and add
+            it to our platform.
+          </span>
+          <button
+            onClick={() => setShowDilogue(false)}
+            className="close__button"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 
 const trendinBusinesss = [
@@ -241,6 +269,83 @@ const trendinBusinesss = [
   },
 ];
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBDHO8OgGVn4_ap5bsz7BRpWk-YpkZUK44",
-})(Profile);
+export default Profile;
+
+// useEffect(() => {
+//   const delay = 2000;
+//   let timerId;
+
+//   const debounceApiCall = () => {
+//     clearTimeout(timerId); // Clear the previous timer
+//     timerId = setTimeout(() => {
+//       // Make your API call here using the searchTerm value
+//       fetchBusinessInfo();
+//       // console.log("API call with search term:", inputValue);
+//     }, delay);
+//   };
+//   if (inputValue.length > 3) {
+//     debounceApiCall();
+//   } else {
+//     setSetSearchResults([]);
+//   }
+//   return () => {
+//     clearTimeout(timerId);
+//   };
+// }, [inputValue]);
+
+// const fetchBusinessInfo = async () => {
+//   setSetSearchingResults(true);
+
+//   const service = new google.maps.places.PlacesService(
+//     document.createElement("div")
+//   );
+//   const request = {
+//     query: inputValue,
+//     // fields: ["name", "formatted_phone_number"],
+//   };
+
+//   service.textSearch(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       if (results && results.length > 0) {
+//         console.log(results);
+//         setSetSearchResults(results);
+//       } else {
+//         setSetSearchingResults(false);
+//       }
+//     } else {
+//       setSetSearchingResults(false);
+//     }
+//   });
+// };
+
+// const getBusinessNumAndOtherDeets = (item) => {
+//   const service = new google.maps.places.PlacesService(
+//     document.createElement("div")
+//   );
+//   const request = {
+//     placeId: item.place_id,
+//     // fields: ["name", "formatted_phone_number"],
+//   };
+
+//   service.getDetails(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       if (results) {
+//         setSelectedBusiness({
+//           name: item.name,
+//           business_status: item.siness_status,
+//           icon: item.icon,
+//           place_id: item.place_id,
+
+//           formatted_address: results.formatted_address,
+//           international_phone_number: results.international_phone_number,
+//           formatted_phone_number: results.formatted_phone_number,
+//         });
+//         setInputValue("");
+//         console.log("getBusinessNumAndOtherDeets", [[item, results]]);
+//       } else {
+//         alert("Error");
+//       }
+//     } else {
+//     }
+//   });
+// };
